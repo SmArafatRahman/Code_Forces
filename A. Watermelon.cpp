3b50d033e22110d4a8a81fba13ecd8d725da8e17
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-02 02:38:43 +0600

    Add files via upload

---
+++ A. Watermelon.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n<4||n%2!=0){
+        cout<<"No\n"<<endl;
+    }
+    else{
+        cout<<"Yes\n"<<endl;
+    }
+
+}
