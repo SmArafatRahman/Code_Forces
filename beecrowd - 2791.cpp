d011a22a7c477ea2f2d5a3c2415e6251db77dab9
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-11 19:39:18 +0600

    Add files via upload

---
+++ beecrowd - 2791.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[4];
+    for(int i=0;i<4;i++){
+        cin>>arr[i];
+        if(arr[i]==1)
+            cout<<i+1<<endl;
+    }
+}
