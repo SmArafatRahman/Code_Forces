a44f94abef5d3c97f4372fd1ecf7d242ce260cf1
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-14 01:43:45 +0600

    Add files via upload

---
+++ 118A - String Task.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]<96)
+            s[i]+=32;
+    }
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+            continue;
+        }
+        else{
+            cout<<"."<<s[i];
+        }
+    }
+}
