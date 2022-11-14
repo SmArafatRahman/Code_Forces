487ed476a070819a554c2b50e45669fdb200245d
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-14 22:33:11 +0600

    Add files via upload

---
+++ 791A - Bear and Big Brother.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,year=0;
+    cin>>a>>b;
+    while(a<=b){
+        year++;
+        a=a*3;
+        b=b*2;
+    }
+    cout<<year<<endl;
+}
