bad99efe06cbcc0b196c7b0558fead8736d9afae
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-14 22:23:37 +0600

    Add files via upload

---
+++ 266A - Stones on the Table.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,c=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0;i<n;i++){
+        if(s[i]==s[i+1])
+            c++;
+    }
+    cout<<c<<endl;
+}
