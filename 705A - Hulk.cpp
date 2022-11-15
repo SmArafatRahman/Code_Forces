98973e17125b7e18d46991f2adb774889b7dfef0
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-15 21:05:21 +0600

    Add files via upload

---
+++ 705A - Hulk.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            if(i!=n){
+                cout<<"I hate that ";
+            }
+            else if(i==n){
+                cout<<"I hate it";
+            }
+
+        }
+        else{
+            if(i!=n){
+                cout<<"I love that ";
+            }
+            else if(n==i){
+                cout<<"I love it";
+            }
+        }
+    }
+}
