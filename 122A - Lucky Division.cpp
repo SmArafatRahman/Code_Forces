a1c096d70c7eeaf39ca28dd9a58ee47111abc992
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-21 02:02:00 +0600

    Add files via upload

---
+++ 122A - Lucky Division.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int n,n1,c=0,temp;
+    cin>>n;
+    if (n % 4 == 0 || n % 7 == 0 || n % 44 == 0 || n % 47 == 0 || n % 74 == 0 || n % 77 == 0 || n % 444 == 0 || n % 447 == 0 || n % 474 == 0 || n % 477 == 0 || n % 744 == 0 || n % 747 == 0 || n % 774 == 0 || n % 777 == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
