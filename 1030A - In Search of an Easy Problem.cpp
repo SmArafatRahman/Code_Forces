48b64c989cf271b7ce3532039df992239699d1f0
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2022-11-23 20:21:52 +0600

    Add files via upload

---
+++ 1030A - In Search of an Easy Problem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,a;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        if(a==1){
+            cout<<"HARD"<<endl;
+            return 0;
+        }
+    }
+    cout<<"EASY"<<endl;
+}
