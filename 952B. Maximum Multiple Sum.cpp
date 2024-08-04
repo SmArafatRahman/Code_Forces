730f8393f618914ccc01c1a8c1fa763d7847e4e5
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2024-08-04 12:07:27 +0600

    Add files via upload

---
+++ 952B. Maximum Multiple Sum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>n;
+        if(n==3){
+            cout<<"3"<<endl;
+        }
+        else{
+            cout<<"2"<<endl;
+        }
+    }
+}
